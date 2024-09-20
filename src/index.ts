55cb29c788619e1a1e8b7c4d112fb35f06995504
Suyash K. <suyu162001@gmail.com>
2024-09-20 21:57:35 +0530

    chore: environment variables

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: [process.env.HOST_URL || "http://localhost:3000"],
     methods: ["GET", "POST"]
   },
   maxHttpBufferSize: 1e8
